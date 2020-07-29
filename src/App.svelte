ff7013e2efca6ccec54374ebf4c11285ffd6ba02
serhiiko <serhiiko@wix.com>
2020-07-29 10:05:33 +0300

    change layout

--- src/App.svelte
+++ src/App.svelte
@@ -5,43 +5,42 @@
 <main class="font_8">
 	<label class="owner">MIKAELA REUBEN | RECIPES</label>
 	<h3 class="title">{$recipe.title}</h3>
-	<div class="ingredients">
-		<h4>INGREDIENTS:</h4>
-		<p>{$recipe.ingredients}</p>
-	</div>
-	<div class="directions">
-		<h4>DIRECTIONS:</h4>
-		<p>{$recipe.directions}</p>
+	<div class="container">
+		<div class="ingredients">
+			<h4>INGREDIENTS:</h4>
+			<p>{$recipe.ingredients}</p>
+		</div>
+		<div class="directions">
+			<h4>DIRECTIONS:</h4>
+			<p>{$recipe.directions}</p>
+		</div>
 	</div>
 </main>
 
 <style>
 	main {
   	display: none;
-		margin-top: 50px;
-		padding: 10%;
-		grid-template-columns: 10% repeat(3, 1fr);
-		gap: 10px;
+		margin-top: 15px;
+		padding: 0 10%;
+	}
+	.container {
+		display: flex;
+		margin-left: 40px;
 	}
 	.owner {
 		letter-spacing: 0.1;
-		font-size: 12px;
-		grid-column: 1/5;
+		font-size: 10px;
 		margin-bottom: 30px
 	}
 	.title {
-		grid-column: 1/5;
 		margin-bottom: 45px;
-	}
-	.ingredients {
-		grid-column: 2/3;
+		font-size: 24px
 	}
 	.directions {
-		grid-column: 3/5;
+		margin-left: 15px;
 	}
 	h4 {
 		font-size: 18px;
-		margin-block-end: 0;
 	}
 	p {
 		padding-block-start: 0.1em;
@@ -51,7 +50,7 @@
 			visibility: hidden;
 		}
 		main {
-			display: grid;
+			display: block;
 			visibility: visible;
 			position: fixed;
 			top: 0;
